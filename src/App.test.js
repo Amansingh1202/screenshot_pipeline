846eb817a4e3b729a7820091a430a22b42b58b51
Amankumar Singh <aks001235@gmail.com>
2022-12-30 13:39:30 +0530

    modified to serve on github pages

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ expect.extend({ toMatchImageSnapshot });
 it('CreateReactApp home', async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('http://localhost:3000');
+    await page.goto('https://amansingh1202.github.io/screenshot_pipeline');
     const image = await page.screenshot();
 
     expect(image).toMatchImageSnapshot();
