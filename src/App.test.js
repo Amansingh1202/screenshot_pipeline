33b0d9ff485617cb7ce48aae8c257ce03e24b849
Amankumar Singh <aks001235@gmail.com>
2022-12-31 18:41:50 +0530

    updated test command to exit after test

--- src/App.test.js
+++ src/App.test.js
@@ -7,5 +7,6 @@ it('CreateReactApp home', async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://amansingh1202.github.io/screenshot_pipeline');
+    // await page.goto('http://localhost:3000/')
     const image = await page.screenshot();
 })
