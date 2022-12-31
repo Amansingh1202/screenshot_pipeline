3b6c4716ea267462eeace25d72d1e413122d4163
Amankumar Singh <aks001235@gmail.com>
2022-12-31 18:59:14 +0530

    updated test to close all await

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,12 @@ const puppeteer = require('puppeteer');
 expect.extend({ toMatchImageSnapshot });
 
 
+
 it('CreateReactApp home', async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://amansingh1202.github.io/screenshot_pipeline');
     // await page.goto('http://localhost:3000/')
     const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot();
 })
