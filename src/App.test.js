59d8b19d87eeb9641d41c0bbde171eb98675af33
Amankumar Singh <aks001235@gmail.com>
2022-12-31 19:13:25 +0530

    updated test to close all await

--- src/App.test.js
+++ src/App.test.js
@@ -2,14 +2,15 @@ import { toMatchImageSnapshot } from 'jest-image-snapshot';
 const puppeteer = require('puppeteer');
 expect.extend({ toMatchImageSnapshot });
 
+jest.setTimeout(5000)
+
 
 
 it('CreateReactApp home', async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://amansingh1202.github.io/screenshot_pipeline');
-    // await page.goto('http://localhost:3000/')
+    //await page.goto('http://localhost:3000/')
     const image = await page.screenshot();
     expect(image).toMatchImageSnapshot();
-    done();
 })
